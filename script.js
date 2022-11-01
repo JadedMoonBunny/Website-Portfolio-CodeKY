8a25ffbc61322dd956ed844ccb15b10753a237aa
Jade Farmer <JadeLewis4499@gmail.com>
2022-11-01 18:47:12 -0500

    "Fixed gallery, removed and added images"

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
