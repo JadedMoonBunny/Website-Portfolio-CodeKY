9963d4e4bfd508eaf4b7ac6e8166781235b3346d
Jade Farmer <JadeLewis4499@gmail.com>
2022-11-10 19:54:21 -0500

    "Contact Page and form have been made."

--- script.js
+++ script.js
@@ -12,5 +12,22 @@ function myFunction() {
   } else {
     x.style.display = "none";
   }
-} 
+}
+
+function sendEmail(){
+  Email.send({
+Host : "smtp.elasticemail.com",
+Username : "JadeLewis4499@gmail.com",
+Password : "Hidalgo102",
+To : 'JadeLewis4499@gmail.com',
+From : document.getElementById("email").value,
+Subject : "New Contact Form Enquiry",
+Body : "Name: " + document.getElementById("name").value
++ "<br> Email: " + document.getElementById("email").value
++ "<br> Phone no: " + document.getElementById("phone").value
++ "<br> Message: " + document.getElementById("message").value
+}).then(
+message => alert("Message Sent Succesfully")
+);
+}
 
